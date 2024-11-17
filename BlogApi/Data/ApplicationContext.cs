12ab3589d806d77e4909abc3b4c0c533f5c55bab
Rerup <jacob@techdrobe.com>
2024-11-17 12:26:50 +0100

    Add ApplicationContext with Migrations for Blog Domain

---
+++ BlogApi/Data/ApplicationContext.cs
@@ -0,0 +1,18 @@
+using System;
+using BlogApi.Domain.BlogDomain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogApi.Data;
+
+public class ApplicationContext : DbContext
+{
+
+    public DbSet<Blog> Blog { get; set; }
+
+    public DbSet<Comment> Comments { get; set; }
+
+    public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
+}
---
+++ BlogApi/Migrations/20241117110035_BlogDomainMigration.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class BlogDomainMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Blog",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "string", maxLength: 30, nullable: false),
+                    Content = table.Column<string>(type: "text", maxLength: 1000, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "DateTime2", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    UpdatedAt = table.Column<DateTime>(type: "DateTime2", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blog", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    BlogId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Content = table.Column<string>(type: "string", maxLength: 255, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "DateTime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "DateTime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Comments_Blog_BlogId",
+                        column: x => x.BlogId,
+                        principalTable: "Blog",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_BlogId",
+                table: "Comments",
+                column: "BlogId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comments");
+
+            migrationBuilder.DropTable(
+                name: "Blog");
+        }
+    }
+}
