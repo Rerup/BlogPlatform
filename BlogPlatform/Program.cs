387c7e3b9a1f4bbd4b0a085d699b59f1af2a28ef
Rerup <jacob@techdrobe.com>
2025-01-17 14:01:50 +0100

    Add Launch configuration

--- BlogPlatform/Program.cs
+++ BlogPlatform/Program.cs
@@ -22,7 +22,7 @@ public class Program
         builder.Services.AddScoped(sp =>
         new HttpClient
         {
-            BaseAddress = new Uri(builder.Configuration["FrontendUrl"] ?? "http://blogapi:8080/api/v1")
+            BaseAddress = new Uri(builder.Configuration["FrontendUrl"] ?? "https://localhost:7195/api/v1")
         });
 
         var app = builder.Build();
